ee9e194a53e888ead94b94e7b0e7c761ae82e01c
rashell <rashellfernandez6401@gmail.com>
2022-09-20 07:40:52 -0400

    Creado el proyecto inicial

--- App.js
+++ App.js
@@ -1,20 +1,45 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StyleSheet, Text, View, Image,Button, Alert,ImageBackground } from 'react-native';
+import BG from './assets/bg.jpg';
+import LOGO from './assets/logo.png';
+const image = { source :BG};
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <ImageBackground source={image} resizeMode="cover" style={styles.imageF}>
+      <Text style={styles.title}>Bienvenido a SCOM REST!</Text>
+      <Image
+        source={LOGO} style={styles.image} />
+      <Button
+        color="blue"
+        title="Ingresar"
+        onPress={() => Alert.alert('Ingresar')}
+      
+      />
+       <Button
+        color="#292929 "
+        title="Registrar"
+        onPress={() => Alert.alert('Registrar')}
+      
+      />
+
+      </ImageBackground>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flex: 1,  
+    backgroundColor:'#fff',
   },
+  imageF: {
+      flex: 1,
+      justifyContent: "center"
+    },
+  title: { fontSize:30, color: '#000'},
+  image: { height: 100, width: 600, justifyContent:'flex-start' },
+  
 });
+
